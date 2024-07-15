fe1d5a362d819c40a64046b35958474644ab466d
Deverson Nunes <deversonnunes10@gmail.com>
2024-07-15 20:36:21 -0300

    arquivo adcionado

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,17 +5,17 @@ import { user } from "/src/scripts/objects/user.js"
 import { screen } from "/src/scripts/objects/screen.js"
 import { userEvents } from "/src/scripts/services/events.js"
 
-// faser a chamada do elemento do input tapy butoon 
+ 
 document.getElementById('btn-search').addEventListener('click',()=>{
-    // chamar o elemento do input e o valor
+   
     const userName= document.getElementById('input-search').value
-    // chamar a funcao
+    
     if(validationInput(userName)) return
     getUserData(userName)
 })
 
 document.getElementById('input-search').addEventListener('keyup', (e)=>{
-    // aq eu ja estou dentro input 
+    
     const userName= e.target.value
     const key= e.which || e.keyCode
     const isEnterKeyPressed= key === 13
