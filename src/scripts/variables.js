eaecd7a981fe44b673ef8f558f434aa1980dd067
Deverson Nunes <deversonnunes10@gmail.com>
2024-07-15 21:14:35 -0300

    first commit

--- src/scripts/variables.js
+++ src/scripts/variables.js
@@ -1,7 +1,7 @@
 const baseUrl = 'https://api.github.com/users'
 
 const repositoriesQuantity = 20
-console.log(repositoriesQuantity)    
+  
 const eventsQuantity =10
 
 export {baseUrl, repositoriesQuantity, eventsQuantity}
